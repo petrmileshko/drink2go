dc6525156f804b82d3e09583d842698104b4541f
Peter <mileshko.petr@yandex.ru>
2024-04-25 15:54:35 +0300

    updated

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,28 +1,34 @@
 /* в этот файл добавляет скрипты*/
 import {
   onLoadJs
-} from './utils/utils.js';
-import menuMobile from './menu/menu.js';
+} from "./utils/utils.js";
+import menuMobile from "./menu/menu.js";
 
 // подготовка элементов шапки сайта для работы когда JS включен и нет сбоя
 onLoadJs(
-  '.page__header',
-  '--nojs'
+  ".page__header",
+  "--nojs"
+)();
+
+// подготовка элементов карты для работы когда JS включен и нет сбоя
+onLoadJs(
+  ".map",
+  "--nojs"
 )();
 
 // Инициализация меню для мобильной версии
 menuMobile({
-  element: '.page__header',
-  activation: 'page__header--opened'
+  element: ".page__header",
+  activation: "page__header--opened"
 }, {
-  element: '.menu',
-  activation: 'menu--opened'
+  element: ".menu",
+  activation: "menu--opened"
 }, {
-  element: '.burger__icon',
-  activation: 'burger__icon--opened'
+  element: ".burger__icon",
+  activation: "burger__icon--opened"
 }, {
-  element: 'menu__link',
-  activation: 'menu__link--current'
+  element: "menu__link",
+  activation: "menu__link--current"
 })();
 
 const swiper = new Swiper(".promo__slider", {
@@ -36,3 +42,30 @@ const swiper = new Swiper(".promo__slider", {
     clickable: true,
   },
 });
+
+const map = L.map("map", {
+  center: [59.96831, 30.31748],
+  zoomControl: false,
+  zoom: 20
+});
+
+L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: "./images/map/map-pin.svg",
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+});
+
+const mainPinMarker = L.marker({
+  lat: 59.96831,
+  lng: 30.31748,
+}, {
+  icon: mainPinIcon,
+}, );
+
+mainPinMarker.addTo(map);
